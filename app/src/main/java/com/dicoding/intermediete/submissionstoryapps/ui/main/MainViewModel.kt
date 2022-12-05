055c3afde41c1b9d36df781f2cedd68323e1c2dd
Muhammad Hakam Fardana <mhakamf13@gmail.com>
2022-12-05 21:37:01 +0700

    Update #4 Fix

--- app/src/main/java/com/dicoding/intermediete/submissionstoryapps/ui/main/MainViewModel.kt
+++ app/src/main/java/com/dicoding/intermediete/submissionstoryapps/ui/main/MainViewModel.kt
@@ -1,16 +1,9 @@
 package com.dicoding.intermediete.submissionstoryapps.ui.main
 
-import android.util.Log
 import androidx.lifecycle.*
-import com.dicoding.intermediete.submissionstoryapps.data.local.StoryModel
 import com.dicoding.intermediete.submissionstoryapps.data.local.UserModel
 import com.dicoding.intermediete.submissionstoryapps.data.local.UserPreference
-import com.dicoding.intermediete.submissionstoryapps.data.remote.network.ApiConfig
-import com.dicoding.intermediete.submissionstoryapps.data.remote.response.GetAllStoryResponse
 import kotlinx.coroutines.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class MainViewModel(private val pref: UserPreference): ViewModel() {
 
--- app/src/main/java/com/dicoding/intermediete/submissionstoryapps/ui/story/AddNewStoryViewModel.kt
+++ app/src/main/java/com/dicoding/intermediete/submissionstoryapps/ui/story/AddNewStoryViewModel.kt
@@ -1,19 +1,11 @@
 package com.dicoding.intermediete.submissionstoryapps.ui.story
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.dicoding.intermediete.submissionstoryapps.data.local.UserModel
 import com.dicoding.intermediete.submissionstoryapps.data.local.UserPreference
-import com.dicoding.intermediete.submissionstoryapps.data.remote.network.ApiConfig
-import com.dicoding.intermediete.submissionstoryapps.data.remote.response.AddNewStoryResponse
-import okhttp3.MultipartBody
-import okhttp3.RequestBody
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class AddNewStoryViewModel(private val pref: UserPreference): ViewModel() {
 
